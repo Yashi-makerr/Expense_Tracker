4c88cc08084af544bd145103c74908fd6bda1376
Yashi-makerr <ykesarwani41@gmail.com>
2026-04-21 22:57:07 +0530

    fix api instance bug

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,15 +1,17 @@
 import axios from "axios";
-const API = import.meta.env.VITE_API_URL;
-axios.create({
-  baseURL: API
-})
+
+const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+});
 
 // attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
+
   return req;
 });
 
