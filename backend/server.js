fb64d5b2ea0b2c86c16cb5f16f71e3e2d18be2b9
Yashi-makerr <ykesarwani41@gmail.com>
2026-04-21 21:58:53 +0530

    fixed server config for deployment

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.CLIENT_URL || "*",
   credentials: true
 }));
 app.use(express.json());
